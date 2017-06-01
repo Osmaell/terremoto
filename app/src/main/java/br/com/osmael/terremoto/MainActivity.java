6bed6e7a5e4596c8c456e8a19dca2ca916fe31b5
Osmael de Sousa Braga <osmaelsousa.ti@gmail.com>
2017-05-31 21:21:27 -0300

    Classe BuscaUtil criada
    Classe faz todo o trabalho de comunicação com os servidores do USGS e converter a resposta em um JSON que será extraido como uma lista

--- app/src/main/java/br/com/osmael/terremoto/MainActivity.java
+++ app/src/main/java/br/com/osmael/terremoto/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.osmael.terremoto;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
 
 import java.util.List;
@@ -12,6 +13,9 @@ import br.com.osmael.terremoto.utils.BuscaUtil;
 
 public class MainActivity extends AppCompatActivity {
 
+    /** URL  para o conjunto de dados de terremoto do USGS */
+    public static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,16 +23,16 @@ public class MainActivity extends AppCompatActivity {
 
         // List criada a partir de lista fornecida pelo método estático
         // BuscaUtil.obterTerremotos()
-        List<Terremoto> terremotos = BuscaUtil.obterTerremotos();
+//        List<Terremoto> terremotos = BuscaUtil.obterTerremotos(USGS_REQUEST_URL);
 
-        ListView listView = (ListView) findViewById(R.id.list_view);
+//        ListView listView = (ListView) findViewById(R.id.list_view);
 
         // Adaptador responsável por adaptar os dados para a ListView
-        TerremotoAdapter adapter = new TerremotoAdapter(
-                this,
-                terremotos);
+//        TerremotoAdapter adapter = new TerremotoAdapter( this, terremotos);
 
-        listView.setAdapter(adapter);
+//        listView.setAdapter(adapter);
+
+        Log.v("OSMAEL", BuscaUtil.obterTerremotos(USGS_REQUEST_URL).toString());
 
     }
 
